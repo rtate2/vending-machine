8612689b17ffa7af21eada233bfd360ccc5294e5
Randy Tate <randytatejr@gmail.com>
2019-11-02 12:32:16 -0500

    getting snackPosition by utilizing machineId

--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,17 +1,21 @@
 import machineData from './machineData';
 import positionData from './positionData';
+import snackPositionData from './snackPositionData';
 
 const getCompleteMachine = () => new Promise((resolve, reject) => {
-  machineData.getMachine()
-  // 1. getMachines - returns first machine (hard coding)
-  // 2. use machineId to get all positions for that machine
-  // 3. use machineId to get all snack positions
+  // 1. getMachines - returns first machine (hard coding) - DONE
+  // 2. use machineId to get all positions for that machine - DONE
+  // 3. use machineId to get all snack positions - DONE
   // 4. use uId of snackPositions/positions to get available snacks for that machine
   // 5. SMASH EM' - return an array of postiions (in order A1, A2, A3, B1 ...) so positions
   // should have position.snack if a snack exists at that posiiton
+  machineData.getMachine()
   // chaining beginning
     .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
-    .then((positions) => resolve(positions))
+    .then((positions) => {
+      snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
+        .then((snackPositions) => resolve(snackPositions));
+    })
   // chaining ending
     .catch((error) => reject(error));
 });

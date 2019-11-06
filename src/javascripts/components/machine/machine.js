00161a3fc450f5880999790eab4553fae3810bbe
Randy Tate <randytatejr@gmail.com>
2019-11-05 20:36:59 -0600

    created purchase function

--- src/javascripts/components/machine/machine.js
+++ src/javascripts/components/machine/machine.js
@@ -1,8 +1,20 @@
+import $ from 'jquery';
+
 import smash from '../../helpers/data/smash';
 import utilities from '../../helpers/utilities';
 import snacks from '../snacks/snacks';
 
 import './machine.scss';
+import snackData from '../../helpers/data/snackData';
+
+const buySnack = (e) => {
+  e.stopImmediatePropagation();
+  const snackId = e.target.id.split('buy-')[1];
+  snackData.buySnack(snackId)
+    // eslint-disable-next-line no-use-before-define
+    .then(() => buildTheMachine())
+    .catch((error) => console.error(error));
+};
 
 const buildTheMachine = () => {
   smash.getCompleteMachine()
@@ -14,6 +26,7 @@ const buildTheMachine = () => {
       });
       domString += '</div>';
       utilities.printToDom('machine', domString);
+      $('#machine').on('click', '.buy-snack', buySnack);
     })
     .catch((error) => console.error(error));
 };
